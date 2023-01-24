845dc5b6cdabbc1de51e534357f25e78615de390
Isaac Bastos <isaacbilac920@gmail.com>
2023-01-24 10:33:21 -0300

    Beta v1

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
 
 let altura = 0
 let largura = 0
+let vidas = 1
+let tempo = 10
+
+let criaMosquitoTempo = 1500
+
+let nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if (nivel === 'normal' ) {
+    criaMosquitoTempo = 1500
+}
+else if (nivel === 'dificil') {
+    criaMosquitoTempo = 1000
+}
+else if (nivel === 'impossivel') {
+    criaMosquitoTempo = 750
+}
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
@@ -9,8 +26,28 @@ function ajustaTamanhoPalcoJogo() {
 
 ajustaTamanhoPalcoJogo()
 
+let cronometro = setInterval(function(){
+    tempo -= 1
+    if (tempo < 0) {
+        clearInterval(cronometro)
+        clearInterval(criaMosca)
+        window.location.href = 'vitoria.html'
+    } else {
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+}, 1000)
 
 function posicaoRandomica() {
+    if (document.getElementById('mosca')) {
+        document.getElementById('mosca').remove()
+        if (vidas > 3) {
+            window.location.href = 'fim_de_jogo.html'
+        }
+        else {
+            document.getElementById("v" + vidas).src = "imagens/coracao_vazio.png"
+            vidas++
+        }
+    }
     let posicaoX = Math.floor(Math.random() * largura) - 90
     let posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -19,11 +56,15 @@ function posicaoRandomica() {
 
     let mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosca.png'
-    mosquito.className = (tamanhoAleatorio() + ' ' + ladoAleatorio())
+    mosquito.className = `${tamanhoAleatorio()} ${ladoAleatorio()}`
     mosquito.style.left = `${posicaoX}px`
     mosquito.style.top = `${posicaoY}px`
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosca'
 
+    mosquito.onclick = function () {
+        this.remove()
+    }
     document.body.appendChild(mosquito)
 }
 
@@ -33,10 +74,8 @@ function tamanhoAleatorio() {
     switch (classe) {
         case 0:
             return 'mosquito'
-        
         case 1:
             return 'mosquito1'
-        
         case 2:
             return 'mosquito2'
     }
@@ -48,7 +87,7 @@ function ladoAleatorio() {
     switch (classe) {
         case 0:
             return 'ladoA'
-        
+
         case 1:
             return 'ladoB'
     }
